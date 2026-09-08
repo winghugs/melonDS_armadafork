1241802e9b98304cc6871fa723cb40ec9ca9dd8d
Kin <kip@kindrake.com>
2026-09-08 16:55:55 -0700

    screenSwap is a bool, dummy

--- src/frontend/qt_sdl/drm_lease/DrmLeaseScreen.cpp
+++ src/frontend/qt_sdl/drm_lease/DrmLeaseScreen.cpp
@@ -191,7 +191,7 @@ bool DrmLeaseScreen::initialize(const std::string& connectorName, int rotation,
 
     auto& cfg = emuInstance->getMainWindow()->getWindowConfig();
     filter = cfg.GetBool("ScreenFilter");
-    screenSwap = cfg.GetBool("ScreenSwap");
+    bool screenSwap = cfg.GetBool("ScreenSwap");
 
     layout.Setup(mode.hdisplay, mode.vdisplay,
                  screenLayout_Natural,
